3108fec149063d078d3afd304e7a427c10ee483c
msingbai <SparkLast@163.com>
2025-03-06 03:22:11 +0800

    更新内容 + Action 自动部署

--- masterpush.sh
+++ masterpush.sh
@@ -1,9 +1,3 @@
-git checkout master
-git checkout --orphan temp
-git rm -rf .
-git commit -m "Clear all files"
-git push origin temp:master --force
-git branch -d temp
 cd E:/VueCode/Blog/msingbai.github.io
 git init
 git remote add origin https://github.com/msingbai/msingbai.github.io.git
