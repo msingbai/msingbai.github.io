522dc7fcf206d77e15f32fa6e69d63984eb49151
msingbai <SparkLast@163.com>
2025-03-06 04:15:24 +0800

    Initial commit with new content

---
+++ docs/.vuepress/nav/frontend.js
@@ -0,0 +1,18 @@
+// vuepress导航栏
+module.exports = {
+    text: "Vuepress",
+    link: "/frontend/",
+    items: [
+        // {
+        //     text: "Vuepress",
+        //     link: "/vuepress/",
+        // },
+        {
+            text: "前端",
+            items: [
+                { text: "Vue", link: "/vue/" },
+                { text: "VuePress", link: "/vuepress/" },
+            ],
+        },
+    ],
+};
