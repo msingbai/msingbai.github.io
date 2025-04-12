4822ea3fe1af981a951916e0edcba620795da5f1
msingbai <SparkLast@163.com>
2025-04-13 04:11:15 +0800

    Initial commit with new content

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -40,7 +40,7 @@ module.exports = {
     // "https://cdn.jsdelivr.net/gh/xingcxb/blog_img@blog1/blog/background/62691402_p0.jpg",
     // "https://cdn.jsdelivr.net/gh/xingcxb/blog_img@blog1/blog/background/63876132_p0.jpg",
   ], // body背景大图(即整个页面的背景图)，默认无。 单张图片 String || 多张图片 Array, 多张图片时每隔15秒换一张。
-  bodyBgImgOpacity: 0.6, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
+  bodyBgImgOpacity: 1, // body背景图透明度，选值 0 ~ 1.0, 默认0.5
 
   // titleBadge: false, // 文章标题前的图标是否显示，默认true
   // titleBadgeIcons: [ // 文章标题前图标的地址，默认主题内置图标
